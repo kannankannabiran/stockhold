bcc0e9f95a2031c835b21bfc10f2b74e8b396469
kannankannabiran <kannankannabiran@gmail.com>
2025-08-10 12:40:02 +0530

    Payment Page Data Added by Kannan

--- src/app/content_data/paymentData.js
+++ src/app/content_data/paymentData.js
@@ -12,7 +12,20 @@ const paymentProducts = [
     description:
       "Hey there! So glad you stopped by to Meet Our Company. Don’t miss out on this opportunity to learn about what we do and the amazing team that makes it all happen! Our company is all about creating innovative solutions and providing top-notch services to our clients. From start to finish, we’re dedicated to delivering results that exceed expectations.",
     signupLink: "/signup",
-  }
+  },
+    {
+    id: "Longtermstock",
+    image: "/longtermstockscourse.svg", // you can also import in the page if you prefer
+    title: (
+      <>
+        How to Select <span className="text-green-400">Long Term Stocks</span> 
+      </>
+    ),
+    price: 4999,
+    description:
+      "Hey there! So glad you stopped by to Meet Our Company. Don’t miss out on this opportunity to learn about what we do and the amazing team that makes it all happen! Our company is all about creating innovative solutions and providing top-notch services to our clients. From start to finish, we’re dedicated to delivering results that exceed expectations.",
+    signupLink: "/signup",
+  },
 ];
 
 export default paymentProducts;

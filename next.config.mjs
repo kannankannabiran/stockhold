931176e544471ce56055c51a307d98822c166ffe
root <root@vmi2773967.contaboserver.net>
2025-08-27 16:44:24 +0200

    Long term Stocks Data Added by Kannan

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,18 @@
+const dev = process.env.NODE_ENV !== 'production';
+
 const nextConfig = {
+  assetPrefix: dev ? '' : 'https://stockhold.in', // use HTTPS in production
   async rewrites() {
     return [
       {
-        source: "/purchases.json",
-        destination: "/data/purchases.json",
+        source: '/api/:path*',
+        destination: dev
+          ? 'http://localhost:3000/:path*'       // dev backend
+          : 'https://stockhold.in/api/:path*',   // production API over HTTPS
       },
     ];
   },
 };
 
 export default nextConfig;
+

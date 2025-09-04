513e138790e75e460ea0e02a26ddab2a95e94ea9
Chandra Mohan <nchandramohan1986@gmail.com>
2025-09-04 23:16:19 +0530

    updated the timeout issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
-const dev = process.env.NODE_ENV !== 'production';
+  GNU nano 7.2                                                                                     next.config.mjs                                                                                               const dev = process.env.NODE_ENV !== 'production';
 
 const nextConfig = {
   assetPrefix: dev ? '' : 'https://stockhold.in', // use HTTPS in production
+  experimental: {
+    serverComponentsExternalPackages: ['yahoo-finance2']
+  },
+  api: {
+    responseLimit: false,
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
+  },
   async rewrites() {
     return [
       {
@@ -12,7 +21,19 @@ const nextConfig = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: '/api/long-data',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
-

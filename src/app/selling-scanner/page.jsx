ea15b26cddcf3ff09ffd879f3162e8f0d0f7ae93
Kannabiran <kannankannabiran@gmail.com>
2026-08-02 19:04:43 +0530

    selling scanner page bug Fixed by Kannan

--- src/app/selling-scanner/page.jsx
+++ src/app/selling-scanner/page.jsx
@@ -476,6 +476,10 @@ export default function CPRScannerPage() {
                     onClick={openDatePicker}
                   >
                     <span className="pointer-events-none font-mono">{selectedDate}</span>
+                    {/* CALENDAR ICON ADDED HERE */}
+                    <svg className="w-4 h-4 text-gray-500 pointer-events-none" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                    </svg>
                     <input
                       ref={dateInputRef}
                       type="date"
